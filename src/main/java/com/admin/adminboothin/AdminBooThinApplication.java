17541148805cd132511892278fdb74aa20d92310
xuben <xuben@zmeng123.com>
2024-04-10 18:46:02 +0800

    init project

---
+++ src/main/java/com/admin/adminboothin/AdminBooThinApplication.java
@@ -0,0 +1,13 @@
+package com.admin.adminboothin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminBooThinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminBooThinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/admin/adminboothin/AdminBooThinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.adminboothin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminBooThinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
